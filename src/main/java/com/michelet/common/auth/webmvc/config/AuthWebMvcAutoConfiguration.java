ab74efbc8100abf584ca145708761be86d210e96
Sehi55 <sehi1028@naver.com>
2026-04-30 17:21:19 +0900

    fix: add @ConditionalOnMissingBean

--- src/main/java/com/michelet/common/auth/webmvc/config/AuthWebMvcAutoConfiguration.java
+++ src/main/java/com/michelet/common/auth/webmvc/config/AuthWebMvcAutoConfiguration.java
@@ -3,6 +3,7 @@ package com.michelet.common.auth.webmvc.config;
 import com.michelet.common.auth.webmvc.aop.AuthorizationAspect;
 import com.michelet.common.auth.webmvc.web.UserContextInterceptor;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,10 +11,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @AutoConfiguration
 public class AuthWebMvcAutoConfiguration {
     @Bean
+    @ConditionalOnMissingBean
     public UserContextInterceptor userContextInterceptor(){
         return new UserContextInterceptor();
     }
     @Bean
+    @ConditionalOnMissingBean
     public AuthorizationAspect authorizationAspect(){
         return new AuthorizationAspect();
     }

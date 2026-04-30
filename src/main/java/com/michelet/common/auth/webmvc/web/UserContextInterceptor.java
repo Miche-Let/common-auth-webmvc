c78236aa91421655c5b299d55302561edfbbca0c
Sehi55 <sehi1028@naver.com>
2026-04-30 18:52:50 +0900

    feat: 주석 추가

--- src/main/java/com/michelet/common/auth/webmvc/web/UserContextInterceptor.java
+++ src/main/java/com/michelet/common/auth/webmvc/web/UserContextInterceptor.java
@@ -19,6 +19,9 @@ public class UserContextInterceptor implements HandlerInterceptor {
         String userId = normalize(request.getHeader(AuthHeaders.USER_ID));
         String roleValue = normalize(request.getHeader(AuthHeaders.USER_ROLE));
 
+        System.out.println("[service] userId = " + userId);
+        System.out.println("[service] roleValue = " + roleValue);
+
         if(userId!=null)
             UserContextHolder.set(new UserContext(userId,parseRole(roleValue)));
 
